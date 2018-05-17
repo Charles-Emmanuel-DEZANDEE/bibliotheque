150dec7c02ad5d11663677fe3de655b188b49d58
cdezandee <cdezandee@sigma.fr>
2018-05-17 17:04:41 +0200

    authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import * as firebase from 'firebase';
 
 @Component({
@@ -9,12 +9,12 @@ import * as firebase from 'firebase';
 export class AppComponent {
   constructor() {
     var config = {
-      apiKey: "AIzaSyBqxB2A5Kx7s-UvX3KlSIG_9aWq4atITno",
-      authDomain: "bibliotheque-cc779.firebaseapp.com",
-      databaseURL: "https://bibliotheque-cc779.firebaseio.com",
-      projectId: "bibliotheque-cc779",
-      storageBucket: "bibliotheque-cc779.appspot.com",
-      messagingSenderId: "782872614156"
+      apiKey: 'AIzaSyBqxB2A5Kx7s-UvX3KlSIG_9aWq4atITno',
+      authDomain: 'bibliotheque-cc779.firebaseapp.com',
+      databaseURL: 'https://bibliotheque-cc779.firebaseio.com',
+      projectId: 'bibliotheque-cc779',
+      storageBucket: 'bibliotheque-cc779.appspot.com',
+      messagingSenderId: '782872614156'
     };
     firebase.initializeApp(config);
   }
